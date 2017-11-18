5bae5a1af991c9c10e2c858804ca3322c0bdd366
Arthur Clemens <arthurclemens@gmail.com>
2017-11-18 22:42:20 +0100

    Combining Polythene and material-components-web (MDC-Web)

---
+++ README.md
@@ -0,0 +1,24 @@
+# Combining Polythene and material-components-web (MDC-Web)
+
+Minimal setup example how to combine [Polythene](https://github.com/ArthurClemens/polythene) components (version Mithril) and MDC-Web components.
+
+## Setup
+
+Init:
+
+~~~
+yarn
+~~~
+
+Start development server (port 3000):
+
+~~~
+yarn run dev
+~~~
+
+Build:
+
+~~~
+yarn run build
+~~~
+
---
+++ scripts/webpack.config.prod.js
@@ -0,0 +1,20 @@
+
+const config = require("./webpack.config.js");
+const webpack = require("webpack");
+const BabiliPlugin = require("babili-webpack-plugin");
+const CompressionPlugin = require("compression-webpack-plugin");
+
+config.plugins.push(new webpack.DefinePlugin({
+  "process.env": { NODE_ENV: JSON.stringify("production") }
+}));
+
+config.plugins.push(new BabiliPlugin());
+config.plugins.push(new CompressionPlugin({
+  asset: "[path].gz[query]",
+  algorithm: "gzip",
+  test: /\.js$|\.css$|\.html$/,
+  threshold: 10240,
+  minRatio: 0
+}));
+
+module.exports = config;
