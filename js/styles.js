e5de6f285b56fb547db5efe012067cd4730d6b0c
Arthur Clemens <arthurclemens@gmail.com>
2019-06-09 21:22:07 +0200

    1.6.2

--- js/styles.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],[,,,,,,,,,function(n,o,c){},function(n,o,c){},function(n,o,c){},function(n,o,c){},function(n,o,c){},function(n,o,c){}]]);
-//# sourceMappingURL=styles.js.map
