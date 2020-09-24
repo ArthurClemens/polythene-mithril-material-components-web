7901122b111e1769cafccacac8e812d910696ce5
Arthur Clemens <arthurclemens@gmail.com>
2020-09-24 22:02:11 +0200

    Update dependencies

--- src/index.js
+++ src/index.js
@@ -16,45 +16,41 @@ import "@material/drawer/dist/mdc.drawer.css";
 import "./styles.css";
 
 const MCWButton = {
-  oncreate: ({ dom }) =>
-    MDCRipple.attachTo(dom),
-  view: ({ attrs }) => 
-    m("button.mdc-button.mdc-button--raised",
-      { onclick: attrs.onclick },
-      [
-        attrs.icon && m("i.material-icons.mdc-button__icon", 
-          attrs.icon
-        ),
-        attrs.label
-      ]
-    )
+  oncreate: ({ dom }) => MDCRipple.attachTo(dom),
+  view: ({ attrs }) =>
+    m("button.mdc-button.mdc-button--raised", { onclick: attrs.onclick }, [
+      attrs.icon && m("i.material-icons.mdc-button__icon", attrs.icon),
+      attrs.label,
+    ]),
 };
 
 const MCWTextField = {
-  oncreate: ({ dom }) =>
-    new MDCTextField(dom),
-  view: () => 
-    m(".mdc-text-field", [
-      m("input.mdc-text-field__input[id='my-text-field'][type='text']"),
-      m("label.mdc-floating-label[for='my-text-field']", 
-        "Your name"
-      ),
-      m(".mdc-line-ripple")
-    ])
+  oncreate: ({ dom }) => new MDCTextField(dom),
+  view: () =>
+    m("label", {"class":"mdc-text-field mdc-text-field--filled mdc-text-field--fullwidth"},
+  [
+    m("span", {"class":"mdc-text-field__ripple"}),
+    m("input", {"class":"mdc-text-field__input","type":"text","placeholder":"Full-Width Text Field","aria-label":"Full-Width Text Field"}),
+    m("span", {"class":"mdc-line-ripple"})
+  ]
+)
 };
 
 const MCWDrawer = {
-  view: () => 
-    m("aside.mdc-drawer.mdc-drawer--dismissible.menu-drawer", 
-      m(".mdc-drawer__content",
-        m("nav.mdc-list",
+  view: () =>
+    m(
+      "aside.mdc-drawer.mdc-drawer--dismissible.menu-drawer",
+      m(
+        ".mdc-drawer__content",
+        m(
+          "nav.mdc-list",
           m(List, {
             className: "drawer-menu",
             header: {
-              title: "Polythene List"
+              title: "Polythene List",
             },
             all: {
-              hoverable: true
+              hoverable: true,
             },
             tiles: [
               { title: "Inbox" },
@@ -62,11 +58,11 @@ const MCWDrawer = {
               { title: "Sent" },
               { title: "Spam" },
               { title: "Trash" },
-            ]
+            ],
           })
         )
       )
-    )
+    ),
 };
 
 const App = {
@@ -79,36 +75,34 @@ const App = {
       m("h1", "Combining Polythene and Material Components for the web"),
       m(".row", [
         m(".header", "Open MCW Drawer from Polythene Button"),
-        m(Button,
-          {
-            raised: true,
-            label: "Open Drawer",
-            events: {
-              onclick: () => {
-                const drawer = MDCDrawer.attachTo(document.querySelector(".menu-drawer"));
-                drawer.open = true;
-                document.body.addEventListener("click", event => {
-                  drawer.open = false;
-                });
-              }
-            }
-          }
-        )
+        m(Button, {
+          raised: true,
+          label: "Open Drawer",
+          events: {
+            onclick: () => {
+              const drawer = MDCDrawer.attachTo(
+                document.querySelector(".menu-drawer")
+              );
+              drawer.open = true;
+              document.body.addEventListener("click", (event) => {
+                drawer.open = false;
+              });
+            },
+          },
+        }),
       ]),
       m(".row", [
         m(".header", "Open Polythene Dialog from MCW Button"),
-        m(MCWButton,
-          {
-            label: "Open Dialog",
-            onclick: () =>
-              Dialog.show({
-                /* note the Dialog component is below the other elements in the app */
-                title: "Polythene Dialog",
-                body: "Click the background to hide, or press ESCAPE.",
-                backdrop: true
-              })
-          }
-        )
+        m(MCWButton, {
+          label: "Open Dialog",
+          onclick: () =>
+            Dialog.show({
+              /* note the Dialog component is below the other elements in the app */
+              title: "Polythene Dialog",
+              body: "Click the background to hide, or press ESCAPE.",
+              backdrop: true,
+            }),
+        }),
       ]),
       m(".row", [
         m(".header", "MCW TextField"),
@@ -116,20 +110,25 @@ const App = {
       ]),
       m(".row", [
         m(".header", "Polythene TextField"),
-        m(TextField, { label: "Your name", floatingLabel: true, id: "polythene" }),
+        m(TextField, {
+          label: "Your name",
+          floatingLabel: true,
+          id: "polythene",
+        }),
       ]),
-      m("div",
-        { class: "footer" },
-        [
-          m("a",
-            { href: "https://github.com/ArthurClemens/polythene-mithril-material-components-web" },
-            "Code on Github"
-          )
-        ]
-      ),
-      m(Dialog)
-    ])
-  ]
+      m("div", { class: "footer" }, [
+        m(
+          "a",
+          {
+            href:
+              "https://github.com/ArthurClemens/polythene-mithril-material-components-web",
+          },
+          "Code on Github"
+        ),
+      ]),
+      m(Dialog),
+    ]),
+  ],
 };
 
 m.mount(document.querySelector("#root"), App);
